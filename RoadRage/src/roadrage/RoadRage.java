f903d45d5788f425b7597d580425c65f2eb433ce
AmishJohn81 <81JOHNBOY@gmail.com>
2015-10-28 20:53:24 -0400

    john  853

--- RoadRage/src/roadrage/RoadRage.java
+++ RoadRage/src/roadrage/RoadRage.java
@@ -8,7 +8,7 @@ public class RoadRage {
     public static void main(String[] args) {
         
      System.out.println("Hello Team! Lets make an awesome App!!!"); //Oscar
-        
+     roadrageproto.RoadRageProto.main(args);
     } //hjhj
     //this is a test from michelle
     //this is a respone from Sarah
