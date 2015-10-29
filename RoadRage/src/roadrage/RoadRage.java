1de6bf1c6211cd39df5a312e8e1f9594e29d60bb
OSSINOVA <oscar.dyremyhr@gmail.com>
2015-10-28 20:48:48 -0400

    jf

--- RoadRage/src/roadrage/RoadRage.java
+++ RoadRage/src/roadrage/RoadRage.java
@@ -7,6 +7,8 @@ public class RoadRage {
     
     public static void main(String[] args) {
         
+     System.out.prinln("Hello Team! Lets make an awesome App!!!");
+        
     } //hjhj
     //this is a test from michelle
     //this is a respone from Sarah
