eba5fb0e90d3dc130f43dabf4559618925ac7d99
jeffreyyeager <jeffreyyeager@104.39.96.207>
2015-10-28 21:00:25 -0400

    added start and end game button

--- RoadRage/src/roadrageproto/MainMenuGUI.java
+++ RoadRage/src/roadrageproto/MainMenuGUI.java
@@ -157,7 +157,7 @@ public class MainMenuGUI extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-        this.dispose();
+        this.setVisible(false);
         GameGUI theGame = new GameGUI();
         theGame.setVisible(true);
         
