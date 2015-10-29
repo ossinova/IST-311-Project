a0d4b0c869e1e9c9985d912c47125c7ff743d1a6
OSSINOVA <oscar.dyremyhr@gmail.com>
2015-10-28 20:49:05 -0400

    added statemnet

--- RoadRage/src/roadrage/RoadRage.java
+++ RoadRage/src/roadrage/RoadRage.java
@@ -7,7 +7,7 @@ public class RoadRage {
     
     public static void main(String[] args) {
         
-     System.out.prinln("Hello Team! Lets make an awesome App!!!");
+     System.out.println("Hello Team! Lets make an awesome App!!!");
         
     } //hjhj
     //this is a test from michelle
